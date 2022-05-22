d296b3e65f2640ad226b6ca133b7f79770675422
m-rusu-st <vasilica.rusu@student.upt.ro>
2022-05-22 15:19:36 +0300

    MT-49 testare PatientsHistory Controller

--- src/test/java/PatientsHistoryControllerTest.java
+++ src/test/java/PatientsHistoryControllerTest.java
@@ -1,11 +1,8 @@
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.loose.fis.sre.services.FileSystemService;
@@ -14,27 +11,12 @@ import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
-import javax.validation.constraints.Null;
 import java.time.DayOfWeek;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(ApplicationExtension.class)
-public class DoctorsHistoryControllerTest {
-
-   /* @BeforeEach
-    void setUp() throws Exception {
-        FileSystemService.APPLICATION_FOLDER = ".registration-example";
-        FileUtils.cleanDirectory(FileSystemService.getApplicationHomeFolder().toFile());
-        UserService.initDatabase();
-        UserService.initDatabase2();
-        UserService.initDatabase3();
-        UserService.addUser("Radoi", "Mirel", "0747851240", "Paltinisanu 22", "RadoiMirel", "radoimirel", "Medic", "Cardiology", "Judetean");
-        UserService.addUser("Pop", "George", "0742258012", "Linistei 20", "PopGeorge", "popgeorge", "Pacient", "", "");
-        UserService.addAppointment("Pop", "George", "0742258012", "PopGeorge", "5/25/2022", DayOfWeek.WEDNESDAY, "12:30", "RadoiMirel");
-
-    }*/
+public class PatientsHistoryControllerTest {
 
     @Start
     void start(Stage primaryStage) throws Exception {
@@ -46,7 +28,7 @@ public class DoctorsHistoryControllerTest {
         UserService.addUser("Radoi", "Mirel", "0747851240", "Paltinisanu 22", "RadoiMirel", "radoimirel", "Medic", "Cardiology", "Judetean");
         UserService.addUser("Pop", "George", "0742258012", "Linistei 20", "PopGeorge", "popgeorge", "Pacient", "", "");
         UserService.addAppointment("Pop", "George", "0742258012", "PopGeorge", "5/25/2022", DayOfWeek.WEDNESDAY, "12:30", "RadoiMirel");
-
+        UserService.addMedicamentation("PopGeorge", "Decasept", "3/day", "5/30/2022", "No!");
 
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         primaryStage.setTitle("Med Tracker");
@@ -57,14 +39,12 @@ public class DoctorsHistoryControllerTest {
     @Test
     void checkHistory(FxRobot robot){
         robot.clickOn("#usernameTextField");
-        robot.write("PopGeorge");
+        robot.write("RadoiMirel");
         robot.clickOn("#enterPasswordField");
-        robot.write("popgeorge");
+        robot.write("radoimirel");
         robot.clickOn("#logInButton");
-        robot.clickOn("#doctorHistoryButton");
+        robot.clickOn("#patientHistoryButton");
 
-        assertEquals(robot.lookup("#tableView").queryTableView().getColumns().get(0).getCellData(0).toString(), "Radoi");
+        assertEquals(robot.lookup("#tableView").queryTableView().getColumns().get(3).getCellData(0).toString(), "Decasept 3/day ");
     }
-
-
 }
